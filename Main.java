511ff595333bf33b4bd91422fb24c4fe042ff005
AlisonChen234 <168639554+AlisonChen234@users.noreply.github.com>
2026-02-12 17:03:04 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -12,24 +12,17 @@ public class Main {
         int twoPairs = 0;
         int onePair = 0;
         int highCards = 0;
-        int rank =0;
 
         ArrayList<String> cardData = getFileData("src/Data");
-
-
-        //convert everything to a num so jack =11, Queen=12, king=13, and Ace=14 and rest is the rest 2=2, 3=3, etc.
-        // Once you convert it then check for the smallest one and give that a rank of 1 and then so fourth until ypu get the largets bid value
-        // loop it for each time as maybe like data points have 2 then check for the second num
-        //then you can keep on like doing it till no 2s, the go tp three
-        // create two arrays one for the cards
-//        // you multiply the rank and the bid value
-//        find total bid value
-
+        int totalBidValue = 0;
 
         for (int i = 0; i < cardData.size(); i++) {
             String line = cardData.get(i);
+
+
             int bar = line.indexOf("|");
             String cards = line.substring(0, bar) + ",";
+            int bid = Integer.parseInt(line.substring(bar + 1));
 
             String[] card = new String[5];
             int position = 0;
@@ -39,94 +32,40 @@ public class Main {
                 position = comma + 1;
             }
 
-            int [] bidValue = new int[3];
+            int currentStrength = calculateStrength(card);
 
-            int totalvalue = totalValue+ rank* bidValue.Of[x]
-
-                if (card.equals("Ace")) { return 14; }
-                else if (card.equals("King")) { return 13; }
-                else if (card.equals("Queen")) { return 12; }
-                else if (card.equals("Jack")) { return 11; }
-                else if (card.equals("10")) { return 10; }
-                else if (card.equals("9")) { return 9; }
-                else if (card.equals("8")) { return 8; }
-                else if (card.equals("7")) { return 7; }
-                else if (card.equals("6")) { return 6; }
-                else if (card.equals("5")) { return 5; }
-                else if (card.equals("4")) { return 4; }
-                else if (card.equals("3")) { return 3; }
-                return 2;
-            }
+            if (currentStrength == 7) fiveOfKind++;
+            else if (currentStrength == 6) fourOfKind++;
+            else if (currentStrength == 5) fullHouse++;
+            else if (currentStrength == 4) threeOfKind++;
+            else if (currentStrength == 3) twoPairs++;
+            else if (currentStrength == 2) onePair++;
+            else highCards++;
 
 
+            int rank = 1;
+            for (int j = 0; j < cardData.size(); j++) {
+                if (i != j) {
+                    String otherLine = cardData.get(j);
+                    int otherBar = otherLine.indexOf("|");
+                    String otherCardsStr = otherLine.substring(0, otherBar) + ",";
 
-            int numFives = 0;
-            int numFours = 0;
-            int numThrees = 0;
-            int numPairs = 0;
-
-
-            // HAVE TO CHANGE EVERYTHING FROM /EQUALS  TO ==
-
-            int count1 = 1;
-            if (card[0]==(card[1])) { count1++; }
-            if (card[0].equals(card[2])) { count1++; }
-            if (card[0].equals(card[3])) { count1++; }
-            if (card[0].equals(card[4])) { count1++; }
-            if (count1 == 5) { numFives++; }
-            if (count1 == 4) { numFours++; }
-            if (count1 == 3) { numThrees++; }
-            if (count1 == 2) { numPairs++; }
+                    String[] otherCard = new String[5];
+                    int otherPos = 0;
+                    for (int b = 0; b < 5; b++) {
+                        int c = otherCardsStr.indexOf(",", otherPos);
+                        otherCard[b] = otherCardsStr.substring(otherPos, c);
+                        otherPos = c + 1;
+                    }
 
 
-            if (!card[1].equals(card[0])) {
-                int count2 = 1;
-                if (card[1].equals(card[2])) { count2++; }
-                if (card[1].equals(card[3])) { count2++; }
-                if (card[1].equals(card[4])) { count2++; }
-                if (count2 == 4) { numFours++; }
-                if (count2 == 3) { numThrees++; }
-                if (count2 == 2) { numPairs++; }
-            }
-
-
-            if (!card[2].equals(card[0]) && !card[2].equals(card[1])) {
-                int count3 = 1;
-                if (card[2].equals(card[3])) { count3++; }
-                if (card[2].equals(card[4])) { count3++; }
-                if (count3 == 3) { numThrees++; }
-                if (count3 == 2) { numPairs++; }
-            }
-
-
-            if (!card[3].equals(card[0]) && !card[3].equals(card[1]) && !card[3].equals(card[2])) {
-                int count4 = 1;
-                if (card[3].equals(card[4])) {
-                    count4++;
-                }
-                if (count4 == 2) {
-                    numPairs++;
+                    if (isStronger(card, otherCard)) {
+                        rank++;
+                    }
                 }
             }
-
-
-            if (numFives == 1) {
-                fiveOfKind++;
-            } else if (numFours == 1) {
-                fourOfKind++;
-            } else if (numThrees == 1 && numPairs == 1) {
-                fullHouse++;
-            } else if (numThrees == 1) {
-                threeOfKind++;
-            } else if (numPairs == 2) {
-                twoPairs++;
-            } else if (numPairs == 1) {
-                onePair++;
-            } else {
-                highCards++;
-            }
+            totalBidValue += bid * rank;
         }
-
 
         System.out.println("Number of five of a kind hands: " + fiveOfKind);
         System.out.println("Number of full house hands: " + fullHouse);
@@ -135,8 +74,89 @@ public class Main {
         System.out.println("Number of two pair hands: " + twoPairs);
         System.out.println("Number of one pair hands: " + onePair);
         System.out.println("Number of high card hands: " + highCards);
+        System.out.println("Total Bid Value: " + totalBidValue);
     }
 
+
+    public static boolean isStronger(String[] hand1, String[] hand2) {
+        int s1 = calculateStrength(hand1);
+        int s2 = calculateStrength(hand2);
+
+        if (s1 != s2) return s1 > s2;
+
+
+        for (int i = 0; i < 5; i++) {
+            int v1 = getVal(hand1[i]);
+            int v2 = getVal(hand2[i]);
+            if (v1 != v2) return v1 > v2;
+        }
+        return false;
+    }
+
+    public static int getVal(String card) {
+        if (card.equals("Ace")) return 14;
+        if (card.equals("King")) return 13;
+        if (card.equals("Queen")) return 12;
+        if (card.equals("Jack")) return 11;
+        if (card.equals("10")) return 10;
+        if (card.equals("9")) return 9;
+        if (card.equals("8")) return 8;
+        if (card.equals("7")) return 7;
+        if (card.equals("6")) return 6;
+        if (card.equals("5")) return 5;
+        if (card.equals("4")) return 4;
+        if (card.equals("3")) return 3;
+        return 2;
+    }
+
+    public static int calculateStrength(String[] card) {
+        int numFives = 0;
+        int numFours = 0;
+        int numThrees = 0;
+        int numPairs = 0;
+
+        int count1 = 1;
+        if (card[0].equals(card[1])) { count1++; }
+        if (card[0].equals(card[2])) { count1++; }
+        if (card[0].equals(card[3])) { count1++; }
+        if (card[0].equals(card[4])) { count1++; }
+        if (count1 == 5) { numFives++; }
+        if (count1 == 4) { numFours++; }
+        if (count1 == 3) { numThrees++; }
+        if (count1 == 2) { numPairs++; }
+
+        if (!card[1].equals(card[0])) {
+            int count2 = 1;
+            if (card[1].equals(card[2])) { count2++; }
+            if (card[1].equals(card[3])) { count2++; }
+            if (card[1].equals(card[4])) { count2++; }
+            if (count2 == 4) { numFours++; }
+            if (count2 == 3) { numThrees++; }
+            if (count2 == 2) { numPairs++; }
+        }
+
+        if (!card[2].equals(card[0]) && !card[2].equals(card[1])) {
+            int count3 = 1;
+            if (card[2].equals(card[3])) { count3++; }
+            if (card[2].equals(card[4])) { count3++; }
+            if (count3 == 3) { numThrees++; }
+            if (count3 == 2) { numPairs++; }
+        }
+
+        if (!card[3].equals(card[0]) && !card[3].equals(card[1]) && !card[3].equals(card[2])) {
+            int count4 = 1;
+            if (card[3].equals(card[4])) { count4++; }
+            if (count4 == 2) { numPairs++; }
+        }
+
+        if (numFives == 1) return 7;
+        if (numFours == 1) return 6;
+        if (numThrees == 1 && numPairs == 1) return 5;
+        if (numThrees == 1) return 4;
+        if (numPairs == 2) return 3;
+        if (numPairs == 1) return 2;
+        return 1;
+    }
 
     public static ArrayList<String> getFileData(String fileName) {
         ArrayList<String> fileData = new ArrayList<String>();
@@ -154,6 +174,4 @@ public class Main {
             return fileData;
         }
     }
-
-
 }
